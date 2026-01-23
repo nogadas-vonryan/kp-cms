96227f7b5c999af5e9ca8d46ba88125b2a633903
nogadas-vonryan <vonnogadas251@gmail.com>
2026-01-23 23:50:30 +0800

    refactor: Update DocumentRepository interface to return Document pointers and adjust List method parameters

--- internal/document/document_repository.go
+++ internal/document/document_repository.go
@@ -5,13 +5,13 @@ import (
 )
 
 type DocumentRepository interface {
-	Create(ctx context.Context, doc *Document) error
-	GetByUUID(ctx context.Context, uuid string) (*DocumentResponse, error)
-	GetByCode(ctx context.Context, code string) (*DocumentResponse, error)
-	GetByFolderName(ctx context.Context, folderName string) (*DocumentResponse, error)
-	Update(ctx context.Context, uuid string, doc *Document) error
+	Create(ctx context.Context, doc *Document) (*Document, error)
+	GetByUUID(ctx context.Context, uuid string) (*Document, error)
+	GetByCode(ctx context.Context, code string) (*Document, error)
+	GetByFolderName(ctx context.Context, folderName string) (*Document, error)
+	Update(ctx context.Context, uuid string, doc *Document) (*Document, error)
 	Delete(ctx context.Context, uuid string) error
-	List(ctx context.Context) ([]*Document, error)
+	List(ctx context.Context, offset int, limit int) ([]*Document, error)
 
 	AddFile(ctx context.Context, uuid string, file File) error
 	DeleteFile(ctx context.Context, uuid string, fileName string) error
