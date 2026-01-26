9273a9d946f16eda826822aef055274d2f75aa9b
nogadas-vonryan <vonnogadas251@gmail.com>
2026-01-26 16:43:53 +0800

    feat: initialize KP Forms plugin and configure pdfMake in main application setup

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,25 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
+import { createPinia, setActivePinia } from 'pinia'
 import router from './router'
 import './style.css'
 import App from './App.vue'
+import { registerCorePlugins } from '@/core/plugins/pluginLoader'
+import { kpFormsPlugin } from '@/plugins/kp-forms/plugin'
+import pdfMake from 'pdfmake/build/pdfmake';
+import pdfFonts from 'pdfmake/build/vfs_fonts'
+
+(pdfMake as any).addVirtualFileSystem(pdfFonts);
+window.pdfMake = pdfMake
 
 const app = createApp(App)
 const pinia = createPinia()
 
 app.use(pinia)
+setActivePinia(pinia)
+
+registerCorePlugins([
+	{ id: 'kpForms', factory: kpFormsPlugin }
+])
 app.use(router)
+
 app.mount('#app')
