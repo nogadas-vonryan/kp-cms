908e8f38c85087ea858556286800aafe64a199ca
nogadas-vonryan <vonnogadas251@gmail.com>
2026-01-29 19:53:26 +0800

    feat: add wailsjs runtime js/ts bundle

---
+++ panel/wailsjs/go/models.ts
@@ -0,0 +1,43 @@
+export namespace main {
+	
+	export class Config {
+	    frontendHost: string;
+	    frontendPort: number;
+	    backendHost: string;
+	    backendPort: number;
+	    username: string;
+	    password: string;
+	    dataPath: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Config(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.frontendHost = source["frontendHost"];
+	        this.frontendPort = source["frontendPort"];
+	        this.backendHost = source["backendHost"];
+	        this.backendPort = source["backendPort"];
+	        this.username = source["username"];
+	        this.password = source["password"];
+	        this.dataPath = source["dataPath"];
+	    }
+	}
+	export class StartBackendServerResult {
+	    generated: boolean;
+	    password: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new StartBackendServerResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.generated = source["generated"];
+	        this.password = source["password"];
+	    }
+	}
+
+}
+
